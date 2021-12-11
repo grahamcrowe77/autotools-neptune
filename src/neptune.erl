6bc36a957f49d4d3cb8a75ebe72cf43de02797b1
Graham Crowe <graham.crowe@telia.com>
2021-12-11 10:40:55 +0100

    Add simple NIF
    Signed-off-by: Graham Crowe <graham.crowe@telia.com>

--- src/neptune.erl
+++ src/neptune.erl
@@ -8,13 +8,34 @@
 
 -endif.
 
--export([main/0, main/1]).
+-export([main/0, main/1, foo/1, bar/1]).
+
+-on_load(init/0).
+
+init() ->
+    PrivDir = case code:priv_dir(?MODULE) of
+		  {error, bad_name} ->
+		      BeamPath = code:where_is_file("neptune.beam"),
+		      BeamDir = filename:dirname(BeamPath),
+		      AppDir = filename:dirname(BeamDir),
+		      filename:join(AppDir, "c_src/.libs");
+		  Dir ->
+		      Dir
+	      end,
+    Path = filename:join(PrivDir, "libneptune_nif"),
+    ok = erlang:load_nif(Path, 0).
 
 main() ->
     io:format("~s~n", [greeting(?DEFAULT_SALUTATION)]).
 
 main(Salutation) ->
     io:format("~s~n", [greeting(Salutation)]).
+
+foo(_X) ->
+    exit(nif_library_not_loaded).
+
+bar(_Y) ->
+    exit(nif_library_not_loaded).
 
 greeting(Salutation) ->
     Place = atom_to_binary(?MODULE),
